1f4493264ab61dab18e76164b3dd0568c8374765
shakilife <shakilife@gmail.com>
2021-12-29 14:35:27 +0530

    bug fix

--- list_all_user/list_user.py
+++ list_all_user/list_user.py
@@ -1,9 +1,10 @@
 import boto3
-aws_mag_con=boto3.session.Session(profile_name="default")
-iam_con_cli=aws_mag_con.client(service_name="iam")
+#aws_mag_con=boto3.session.Session(profile_name="default")
+#iam_con_cli=aws_mag_con.client(service_name="iam")
+iam_con=boto3.client(service_name="iam")
 
 #List all IAM user using client object
 
-response=iam_con_cli.list_users()
+response=iam_con.list_users()
 for each_item in response['Users']:
     print(each_item['UserName'],each_item['UserId'],each_item['Arn'])
