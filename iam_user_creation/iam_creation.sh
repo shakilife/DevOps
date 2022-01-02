b31d59fd52cf29a78166cb76f19729eec9f78e51
shakilife <shakilife@gmail.com>
2022-01-02 08:47:12 +0530

    output update on script

--- iam_user_creation/iam_creation.sh
+++ iam_user_creation/iam_creation.sh
@@ -2,9 +2,9 @@
 
 # AWS cli command to create IAM user
 $1 $2
-aws iam create-user --user-name $1
+aws iam create-user --user-name $1 --output table
 
-aws iam create-login-profile --user-name $1 --password Welcome123 --password-reset-required
+aws iam create-login-profile --user-name $1 --password Welcome123 --password-reset-required --output table
 
-aws iam attach-user-policy --user-name=$1 --policy-arn=$2
+aws iam attach-user-policy --user-name=$1 --policy-arn=$2 --output table
 
