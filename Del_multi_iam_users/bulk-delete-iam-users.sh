e15823b2dd1087d37dfd4f5ecf810a8ce3940dec
shakilife <shakilife@gmail.com>
2022-01-07 17:51:31 +0530

    attach policy to user command remove from scriptv1

--- Del_multi_iam_users/bulk-delete-iam-users.sh
+++ Del_multi_iam_users/bulk-delete-iam-users.sh
@@ -4,20 +4,15 @@ group=$1 arn=$2
 
 aws iam list-users --output table
 
+aws iam detach-group-policy --group-name ${group} --policy-arn ${arn}
+echo "Policy ${arn} has been detach from group ${group}"
+
 for i in `cat users`
 do
   `aws iam remove-user-from-group --user-name "$i" --group-name ${group}; \
   aws iam delete-login-profile --user-name="$i"; \
   aws iam delete-user --user-name "$i"`
 done
-
-#aws iam attach-group-policy --group-name ${group} --policy-arn ${arn}
-
-aws iam detach-group-policy --group-name ${group} --policy-arn ${arn}
-
-echo "Policy ${arn} has been detach from group ${group}"
-
-#aws iam create-group --group-name ${group} --output table
 
 aws iam delete-group --group-name ${group} --output table
 
